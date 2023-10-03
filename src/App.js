1d2d6cf3d5b57fbc68f69671b154424a574739b2
Aubin Dash <kiaubin13@gmail.com>
2023-10-03 23:12:41 +0200

    firs changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [noteTitle, setNoteTitle] = useState("Untitled Note")
+  const [noteContent, setNoteContent] = useState("Empty")
+
+  // title change
+    function TitleChange(e){
+      setNoteTitle(e.target.value)
+    }
+  
+  // Content Change
+    function ContentChange(f){
+      setNoteContent(f.target.value)
+    }
+
+  //Sidebar
+  function Sidebar() {
+    //adding condition for empty note title
+    let localNoteTitle
+    if (noteTitle == "")
+      localNoteTitle = "Untitled Note";
+    else 
+      localNoteTitle = noteTitle;
+
+    // adding condition for empty note content
+    let localNoteContent
+    if (noteContent == "")
+      localNoteContent = "Empty";
+    else 
+      localNoteContent = noteContent;
+
+    return <div className="sidebar">
+      <div className='notes'>
+        <h3>{localNoteTitle}</h3>
+        <p>{localNoteContent}</p>
+      </div>
     </div>
+  }
+
+  // Main Bar
+  function Mainbar() {
+    return <form>
+      <input type='text' value={noteTitle} id='note-title' onChange={TitleChange}></input> 
+      <input type='text' value={noteContent} id='note-content' onChange={ContentChange} placeholder='Write your note...'></input>
+    </form>
+  }
+
+  //Final render
+  return (<div className='container'>
+    {Sidebar()}
+    {Mainbar()}
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log("reached index.js")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
